2bb9a195ef4d46773b61431b96970e4432843931
이승현 <dltmdgus741@gmail.com>
2022-07-18 19:31:53 +0900

    2022-07-18

---
+++ CodingTest/src/진짜시험/T_문자열로숫자합구하기.java
@@ -0,0 +1,54 @@
+package 진짜시험;
+
+public class T_문자열로숫자합구하기 {
+
+    public static void main(String[] args) {
+//        solution("1234", "56"); //1290
+//        solution("56", "1234"); //1290
+//        solution("1234", "5678"); //6912
+//        solution("1111", "8889"); //10000
+        solution("9999", "0"); //9999
+    }
+
+    public static void solution(String a, String b) {
+
+        if (a.equals("0")) {
+            System.out.println(Integer.parseInt(b));
+        } else if (b.equals("0")) {
+            System.out.println(Integer.parseInt(a));
+        }
+
+        StringBuilder answer = new StringBuilder();
+
+        boolean flag = a.length() > b.length();
+        int lenMin = Math.min(a.length(), b.length());
+        int lenA = a.length() - 1;
+        int lenB = b.length() - 1;
+
+        int n = 0;
+
+        for (int i = 0; i < lenMin; i++) {
+            n += (a.charAt(lenA) - '0') + (b.charAt(lenB) - '0');
+            answer.insert(0, (n % 10));
+            n /= 10;
+
+            lenA--;
+            lenB--;
+        }
+
+        if (lenA != lenB) {
+            String str = flag ? a : b;
+            for (int i = lenMin - 1; i >= 0; i--) {
+                n += str.charAt(i) - '0';
+                answer.insert(0, (n % 10));
+                n /= 10;
+            }
+        }
+
+        if (n != 0) {
+            answer.insert(0, n);
+        }
+
+        System.out.println(answer);
+    }
+}
