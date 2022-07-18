0cff011273099c2bd5b10eb203c00cad36102b24
이승현 <dltmdgus741@gmail.com>
2022-07-19 08:38:07 +0900

    chore: 파일명 알고리즘별 분류

--- CodingTest/src/백준/B_DP_10164_격자상의경로.java
+++ CodingTest/src/백준/B_DP_10164_격자상의경로.java
@@ -1,12 +1,8 @@
 package 백준;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
-public class B_10164_격자상의경로 {
+public class B_DP_10164_격자상의경로 {
 
     public static void main(String[] args) throws IOException {
 //        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
