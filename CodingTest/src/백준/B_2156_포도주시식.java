b4b601f517787d4c59cda59f3475566d4e26dbd8
이승현 <dltmdgus741@gmail.com>
2022-07-19 08:27:52 +0900

    2022-07-19 DP B_2156_포도주시식 답안 개선

--- CodingTest/src/백준/B_2156_포도주시식.java
+++ CodingTest/src/백준/B_2156_포도주시식.java
@@ -23,32 +23,17 @@ public class B_2156_포도주시식 {
 
     public static void solution(int N, int[] arr) {
         int[] dp = new int[N + 1];
-        int[] cnt = new int[N + 1];
 
         for (int n = 1; n <= Math.min(2, N); n++) {
             dp[n] = dp[n-1] + arr[n];
-            cnt[n] = cnt[n-1] + 1;
         }
 
         for (int n = 3; n <= N; n++) {
-            if (cnt[n - 1] == 2) {
-                int num1 = dp[n - 1];
-                int num2 = dp[n - 2] + arr[n];
-                int num3 = dp[n - 3] + arr[n - 1] + arr[n];
+            int num1 = dp[n - 1];
+            int num2 = dp[n - 2] + arr[n];
+            int num3 = dp[n - 3] + arr[n - 1] + arr[n];
 
-                dp[n] = Math.max(Math.max(num1, num2), num3);
-
-                if (dp[n] == num1) {
-                    cnt[n] = 0;
-                } else if (dp[n] == num2) {
-                    cnt[n] = 1;
-                } else {
-                    cnt[n] = 2;
-                }
-            } else {
-                dp[n] = dp[n - 1] + arr[n];
-                cnt[n] = cnt[n - 1] + 1;
-            }
+            dp[n] = Math.max(Math.max(num1, num2), num3);
         }
 
         System.out.println(Arrays.toString(dp));
