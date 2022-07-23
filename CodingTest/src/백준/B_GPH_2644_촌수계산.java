e4207ad0e01bc16b65959173499f6945c768678b
이승현 <dltmdgus741@gmail.com>
2022-07-24 02:19:11 +0900

    solve: B_GPH_2644_촌수계산

--- CodingTest/src/백준/B_GPH_2644_촌수계산.java
+++ CodingTest/src/백준/B_GPH_2644_촌수계산.java
@@ -1,4 +1,76 @@
 package 백준;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class B_GPH_2644_촌수계산 {
+
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int n = Integer.parseInt(br.readLine());
+//        StringTokenizer st1 = new StringTokenizer(br.readLine());
+//        int[] question = new int[2];
+//        for (int i=0; i<2; i++) {
+//            question[i] = Integer.parseInt(st1.nextToken());
+//        }
+//        int m = Integer.parseInt(br.readLine());
+//        int[][] relationship = new int[m][2];
+//        for (int i=0; i<m; i++) {
+//            StringTokenizer st2 = new StringTokenizer(br.readLine());
+//            for (int j=0; j<2; j++) {
+//                relationship[i][j] = Integer.parseInt(st2.nextToken());
+//            }
+//        }
+//        solution(n, question, m, relationship);
+
+        solution(9, new int[]{7, 3}, 7, new int[][]{{1,2}, {1,3}, {2,7}, {2,8}, {2,9}, {4,5}, {4,6}}); // 3
+        solution(9, new int[]{8, 6}, 7, new int[][]{{1,2}, {1,3}, {2,7}, {2,8}, {2,9}, {4,5}, {4,6}}); // -1
+        solution(9, new int[]{2, 3}, 7, new int[][]{{1,2}, {1,3}, {2,7}, {2,8}, {2,9}, {4,5}, {4,6}}); // 2
+        solution(11, new int[]{7, 10}, 9, new int[][]{{1,2}, {1,3}, {2,7}, {2,8}, {2,9}, {4,5}, {4,6}, {7,10}, {7,11}}); // 1
+    }
+
+    private static void solution(int n, int[] question, int m, int[][] relationship) {
+        int[] parent = new int[n+1];
+        int answer = 0, i;
+
+        for (i=0; i<m; i++) {
+            parent[relationship[i][1]] = relationship[i][0];
+        }
+
+        int cntX = 0, cntY = 0;
+        for (i = question[0]; parent[i] != 0; i = parent[i]) {
+            cntX++;
+        }
+        for (i = question[1]; parent[i] != 0; i = parent[i]) {
+            cntY++;
+        }
+
+        int younger = cntX > cntY ? question[0] : question[1];
+        if (cntX != cntY) {
+            for (i = 1, younger = parent[younger]; i < Math.abs(cntX - cntY); i++) {
+                younger = parent[younger];
+            }
+        }
+
+        int rootX = cntX > cntY ? younger : question[0];
+        int rootY = cntX <= cntY ? younger : question[1];
+        for (i = 0; i <= Math.min(cntX, cntY); i++) {
+            if (rootX == rootY) {
+                break;
+            }
+            rootX = parent[rootX];
+            rootY = parent[rootY];
+            answer++;
+        }
+
+        if (i > Math.min(cntX, cntY)) {
+            answer = -1;
+        } else {
+            answer = answer * 2 + Math.abs(cntX - cntY);
+        }
+
+        System.out.println(answer);
+    }
 }
--- CodingTest/src/진짜시험/T_문자열로숫자합구하기.java
+++ CodingTest/src/진짜시험/T_문자열로숫자합구하기.java
@@ -3,39 +3,21 @@ package 진짜시험;
 public class T_문자열로숫자합구하기 {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        solution("1234", "56"); //1290
-        solution("56", "1234"); //1290
-        solution("1234", "5678"); //6912
-        solution("1111", "8889"); //10000
-=======
+
 //        solution("1234", "56"); //1290
 //        solution("56", "1234"); //1290
 //        solution("1234", "5678"); //6912
 //        solution("1111", "8889"); //10000
->>>>>>> ec25f7753e984b2c63856bf789ae416c52baba48
         solution("9999", "0"); //9999
     }
 
     public static void solution(String a, String b) {
 
-<<<<<<< HEAD
-=======
-        if (a.equals("0")) {
-            System.out.println(Integer.parseInt(b));
-        } else if (b.equals("0")) {
-            System.out.println(Integer.parseInt(a));
-        }
-
->>>>>>> ec25f7753e984b2c63856bf789ae416c52baba48
         StringBuilder answer = new StringBuilder();
 
         boolean flag = a.length() > b.length();
         int lenMin = Math.min(a.length(), b.length());
-<<<<<<< HEAD
         int lenMax = Math.max(a.length(), b.length());
-=======
->>>>>>> ec25f7753e984b2c63856bf789ae416c52baba48
         int lenA = a.length() - 1;
         int lenB = b.length() - 1;
 
@@ -52,13 +34,8 @@ public class T_문자열로숫자합구하기 {
 
         if (lenA != lenB) {
             String str = flag ? a : b;
-<<<<<<< HEAD
             for (int i = lenMin + 1; i <= lenMax; i++) {
                 n += str.charAt(lenMax - i) - '0';
-=======
-            for (int i = lenMin - 1; i >= 0; i--) {
-                n += str.charAt(i) - '0';
->>>>>>> ec25f7753e984b2c63856bf789ae416c52baba48
                 answer.insert(0, (n % 10));
                 n /= 10;
             }
