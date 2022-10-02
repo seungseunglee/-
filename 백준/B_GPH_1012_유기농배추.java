60be69618bdd352de877f5947ad0fc50cebb3b0d
이승현 <dltmdgus741@gmail.com>
2022-10-02 19:21:54 +0900

    chore: 파일명 변경

--- 백준/B_GPH_1012_유기농배추.java
+++ 백준/B_GPH_1012_유기농배추.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class B_1012_GPH_유기농배추 {
+public class B_GPH_1012_유기농배추 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
